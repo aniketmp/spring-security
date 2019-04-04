60dd7bcc891f2156ca88a139596dbe67515de821
aniket <aniket.paranjpe@pebstone.com>
2019-04-04 15:05:48 +0530

    x509 client authentication added

---
+++ src/main/java/com/pebstone/X509CertificateSecurityConfigurationAdapter.java
@@ -0,0 +1,42 @@
+package com.pebstone;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Order(1)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class X509CertificateSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+          .and()
+          .x509()
+            .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+            .userDetailsService(userDetailsService());
+    }
+ 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                if (username.equals("apotek.no")) {
+                    return new User(username, "", 
+                      AuthorityUtils
+                        .commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+                else return null;
+            }
+        };
+    }
+}
